bee010dfb14118c6c4c80310bf83fa681f936e3c
UtkarshM-Hub <utmandape4@gmail.com>
2022-09-06 10:44:02 +0530

    Added Solution of Flatten doubly linked list

---
+++ Flatten a multilevel doubly linkded list/program.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+
+class Solution {
+    Node* resultHead=NULL;
+    Node* current=NULL;
+public:
+    
+	//create doubly linked list
+    void Add(int val){
+        Node* temp=new Node();
+        temp->val=val;
+        temp->next=NULL;
+        temp->prev=NULL;
+        if(this->resultHead==NULL){
+            this->resultHead=temp;
+            this->current=temp;
+            return;
+        }
+        this->current->next=temp;
+        temp->prev=this->current;
+        this->current=temp;
+    }
+    
+    Node* flatten(Node* head) {
+        stack<Node*> parent;
+        Node* curr=head;
+        
+        while(curr!=NULL || !parent.empty()){
+            if(curr==NULL){
+                curr=parent.top();
+                parent.pop();
+                curr=curr->next;
+                continue;
+            }
+            Add(curr->val);
+            if(curr->child!=NULL){
+                parent.push(curr);
+                curr=curr->child;
+                continue;
+            }
+            if(curr->next==NULL && !parent.empty()){
+                curr=parent.top();
+                parent.pop();
+            }
+            curr=curr->next;
+        }
+        return this->resultHead;
+    };
+};
+int main(){
+    
+}
