7d1ad01cf24506a2605e891df59ea7be7d44ae6b
UtkarshM-Hub <utmandape4@gmail.com>
2022-09-13 09:16:28 +0530

    Added solution of 'remove duplicate from linked list II'

---
+++ Remove Duplicates from Linked List II/program.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:   
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head) return head;
+        ListNode* ret = new ListNode();
+        ret->next = head;
+        ListNode* curr = head, *pre = ret;
+        while(curr and curr->next){
+            if(curr->val == curr->next->val){
+                while(curr->next and curr->val == curr->next->val){
+                    curr = curr->next;
+                }
+                pre->next = curr->next;
+            }else{
+                pre = curr;
+            }
+            curr = curr->next;
+        }
+        return ret->next;
+    }
+};
+
+int main(){
+    
+}
